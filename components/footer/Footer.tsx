05cb6f7f03cd792ed26090f0d2cebb8d70e9b9ab
johnatan <nathanjoh143@gmail.com>
2021-08-10 23:49:37 +0400

    aeijvjvqs dd

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC<FooterProps> = ({
     return (
         <Wrapper className={["footer", className].join(' ')}>
             <div className="container">
-            <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
             </div>
         </Wrapper>
     )
